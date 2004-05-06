fb337cfc4d42b308c332b7290135ac51913bb05f
PClaerho <PClaerho@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2004-05-06 09:25:46 +0000

    The changelog file is now created based on the logfiles from SVN.

--- YDFramework2/doc/make_api.bat
+++ YDFramework2/doc/make_api.bat
@@ -4,3 +4,13 @@ rem Generate the API documentation
 rmdir /Q /S api
 mkdir api
 doxygen docs_api.dxy
+
+rem Generate the changelog files
+cd ..\..
+echo Creating changelog files...
+
+for /f "" %%f in ( 'svnlook youngest C:/_SVNRepos/YDF2' ) do ( set REV=%%f )
+svn log -v -r 1:%REV% > YDFramework2\doc\changelog.txt
+svn log -v --xml -r 1:%REV% > YDFramework2\doc\changelog.xml
+
+cd YDFramework2\doc
