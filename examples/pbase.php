102797b3f34197094e09915b319c2f3419e433c1
PClaerho <PClaerho@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2004-06-05 09:42:02 +0000

    Enhanced the pbase example. You can now very easily browse through the images and you can also get them as an RSS feed.

--- examples/pbase.php
+++ examples/pbase.php
@@ -12,40 +12,134 @@
     require_once( 'YDUrl.php' );
     require_once( 'YDDebugUtil.php' );
     require_once( 'YDArrayUtil.php' );
+    require_once( 'YDFeedCreator.php' );
 
     // Class definition
     class pbaseRequest extends YDRequest {
 
         // Class constructor
         function pbaseRequest() {
+
+            // Initialize the parent
             $this->YDRequest();
+
+            // The list of galleries
+            /*
+            $this->galleries = array(
+                1 => 'http://www.pbase.com/bba/odk2_2004',
+                2 => 'http://www.pbase.com/bba/krab04',
+            );
+            */
+
+            // The fixed url of our gallery
+            $this->url = 'http://www.pbase.com/bba/odk2_2004&page=all';
+            $this->setVar( 'url', $this->url );
+            
+            // The regex pattern to match the images
+            $this->pattern = '/www\.pbase\.com\/image\/([0-9]+)/ism';
+
+            // Get the list of images
+            $objUrl = new YDUrl( $this->url );
+            $contents = $objUrl->getContentsWithRegex( $this->pattern );
+            $this->images = $contents[1];
+
         }
 
         // Default action
         function actionDefault() {
 
-            // Define the pattern
-            $url = 'http://www.pbase.com/bba/odk2_2004&page=all';
-            $pattern = '/www\.pbase\.com\/image\/([0-9]+)/ism';
-
-            // Get the contents
-            $objUrl = new YDUrl( 'http://www.pbase.com/bba/odk2_2004&page=all' );
-            $contents = $objUrl->getContentsWithRegex( $pattern );
-
             // Add the template variables
-            $this->setVar( 'images', YDArrayUtil::convertToTable( $contents[1], 4, true ) );
-            $this->setVar( 'url', $url );
+            $this->setVar( 
+                'images', YDArrayUtil::convertToTable( $this->images, 4, true )
+            );
 
             // Output the template
             $this->outputTemplate();
 
         }
 
+        // Show an image gallery
+        /*
+        function actionShowGallery() {
+
+            // Check if the ID exists
+            if ( ! isset( $_GET['id'] ) ) {
+                $this->redirectToAction();
+            }
+
+            // Check if the ID exists
+            if ( ! in_array( $_GET['id'], array_keys( $this->galleries ) ) ) {
+                $this->redirectToAction();
+            }
+
+            // Output the template
+            $this->outputTemplate();
+
+        }
+        */
+
         // Show an image
-        function actionShow() {
-            $id = $_GET['id'];
-            $url = 'http://www.pbase.com/image/' . $id . '.jpg';
-            $this->redirect( $url );
+        function actionShowImage() {
+
+            // Check if the ID exists
+            if ( ! isset( $_GET['id'] ) ) {
+                $this->redirectToAction();
+            }
+
+            // Check if the ID exists
+            if ( ! in_array( $_GET['id'], $this->images ) ) {
+                $this->redirectToAction();
+            }
+
+            // Get current image number
+            $imageCurrent = array_search( $_GET['id'], $this->images );
+
+            // Start with no previous and next image
+            $imagePrevious = null;
+            $imageNext = null;
+
+            // Get the index of the previous image
+            if ( $imageCurrent != 0 ) {
+                $imagePrevious = $this->images[ $imageCurrent - 1 ];
+            }
+
+            // Get the index if the next image
+            if ( $imageCurrent < sizeof( $this->images ) ) {
+                $imageNext = $this->images[ $imageCurrent + 1 ];
+            }
+
+            // Add them to the template
+            $this->setVar( 'imagePrevious', $imagePrevious );
+            $this->setVar( 'imageCurrent', $_GET['id'] );
+            $this->setVar( 'imageNext', $imageNext );
+
+            // Output the template
+            $this->outputTemplate();
+
+        }
+
+        // Get the list of images as RSS feed
+        function actionRss() {
+
+            // Create a new Feed
+            $this->fc = new YDFeedCreator();
+            $this->fc->setTitle( $this->url );
+            $this->fc->setLink( $this->getCurrentUrl() );
+
+            // Add the images
+            foreach ( $this->images as $image ) {
+
+                $this->fc->addItem(
+                    $image . '.jpg',
+                    'http://www.pbase.com/image/' . $image . '.jpg',
+                    '<img src="http://www.pbase.com/image/' . $image . '.jpg">'
+                );
+
+            }
+
+            // Output the feed
+            $this->fc->outputXml( 'RSS2.0' );
+
         }
 
     }
--- examples/pbase.tpl
+++ examples/pbase.tpl
@@ -8,22 +8,48 @@
 
 <body>
 
-    <h3><?= $url ?></h3>
-
-    <table cellspacing="16" border="0">
-    <?php foreach ( $images as $imagerow ) { ?>
-        <tr>
-            <?php foreach( $imagerow as $image ) { ?>
-                <td width="160" align="center">
-                <?php if ( $image ) { ?>
-                    <a href="<?= $YD_SELF_SCRIPT ?>?do=show&id=<?= $image ?>" target="_blank"><img src="http://www.pbase.com/image/<?= $image ?>/small.jpg" border="1"></a>
-                    <a href="<?= $YD_SELF_SCRIPT ?>?do=show&id=<?= $image ?>" target="_blank"><?= $image ?>.jpg</a>
+    <?php if ( $YD_ACTION == 'default' ) { ?>
+        <h3><?= $url ?></h3>
+        <p><a href="<?= $YD_SELF_SCRIPT ?>?do=rss">image list as RSS</a></p>
+        <table cellspacing="16" border="0">
+        <?php foreach ( $images as $imagerow ) { ?>
+            <tr>
+                <?php foreach( $imagerow as $image ) { ?>
+                    <td width="160" align="center">
+                    <?php if ( $image ) { ?>
+                        <a href="<?= $YD_SELF_SCRIPT ?>?do=ShowImage&id=<?= $image ?>" target="_blank"><img src="http://www.pbase.com/image/<?= $image ?>/small.jpg" border="1"></a>
+                        <a href="<?= $YD_SELF_SCRIPT ?>?do=ShowImage&id=<?= $image ?>" target="_blank"><?= $image ?>.jpg</a>
+                    <?php } ?>
+                    </td>
                 <?php } ?>
-                </td>
+            </tr>
+        <? } ?>
+        </table>
+    <?php } ?>
+
+    <?php if ( $YD_ACTION == 'showimage' ) { ?>
+
+        <h3><?= $imageCurrent ?>.jpg</h3>
+
+        <p>[
+            <?php if ( $imagePrevious ) { ?>
+                <a href="<?= $YD_SELF_SCRIPT ?>?do=ShowImage&id=<?= $imagePrevious ?>">previous</a>
+            <?php } else { ?>
+                previous
             <?php } ?>
-        </tr>
-    <? } ?>
-    </table>
+            |
+            <a href="<?= $YD_SELF_SCRIPT ?>">overview</a>
+            |
+            <?php if ( $imageNext ) { ?>
+                <a href="<?= $YD_SELF_SCRIPT ?>?do=ShowImage&id=<?= $imageNext ?>">next</a>
+            <?php } else { ?>
+                next
+            <?php } ?>
+        ]</p>
+
+        <p><img src="http://www.pbase.com/image/<?= $imageCurrent ?>.jpg" border="1"></p>
+
+    <?php } ?>
 
     <p>[
         <a href="index.php">other samples</a>
