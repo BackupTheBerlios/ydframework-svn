cf9f036c82806b32e884746c394ddeee989da354
david <david@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2004-10-01 21:03:19 +0000

    Changed YDCart to a more independent class. Now the user has to create a sub-class of YDCart with methods to retrieve the product's informations the way he wants.

--- YDFramework2.0/trunk/YDFramework2/YDClasses/YDCart.php
+++ YDFramework2.0/trunk/YDFramework2/YDClasses/YDCart.php
@@ -18,82 +18,23 @@
 		var $item = array();
 
 		/**
-		 *  Array with the cart's options.
-		 *
-		 *  @internal
-		 */
-		var $_options = array();
-
-		/**
 		 *	Class constructor for the YDCart class.
 		 *
-		 *	@param $driver			Name of the cart driver.
 		 *	@param $session_var		(optional) Name to use in the session var - $_SESSION[ $session_var ]
 		 *	@param $options			(optional) Information used by the cart driver.
 		 *
 		 *	@deprecated	 			Use the static method getInstance to get a new YDCart class instance.
 		 */			
-		function YDCart( $driver, $session_var='YDCart', $options=array() ) {
-			trigger_error( 'Use the static method getInstance to get a new YDCart class instance.', YD_ERROR );
-		}
+		function YDCart( $session_var='YDCart' ) {			
 		
-		/**
-		 *	Using this static function, you can get an instance of a YDCartDriver class.
-		 *
-		 *	@param $driver			Name of the cart driver.
-		 *	@param $session_var		(optional) Name to use in the session var - $_SESSION[ $session_var ]
-		 *	@param $options			(optional) Information used by the cart driver.
-		 */
-		function getInstance( $driver, $session_var='YDCart', $options=array() ) {
-		
-			// The list of known drivers
-			$regDrivers = array();
-
-			// Register the standard drives
-			$regDrivers[ strtolower( 'array' ) ] = array(
-				'class' => 'YDCartDriver_array', 'file' => 'YDCartDriver_array.php'
-			);
-			$regDrivers[ strtolower( 'db' ) ] = array(
-				'class' => 'YDCartDriver_db', 'file' => 'YDCartDriver_db.php'
-			);
-//			$regDrivers[ strtolower( 'xml' ) ] = array(
-//				'class' => 'YDCartDriver_xml', 'file' => 'YDCartDriver_xml.php'
-//			);
-
-			// Check if the driver exists
-			if ( ! array_key_exists( strtolower( $driver ), $regDrivers ) ) {
-				trigger_error( 'Unsupported Cart driver: "' . $driver . '".', YD_ERROR );
-			}
-
-			// Include the driver
-			if ( ! empty( $regDrivers[ strtolower( $driver ) ]['file'] ) ) {
-				require_once( 'YDCartDrivers/' . $regDrivers[ strtolower( $driver ) ]['file'] );
-			}
-
-			// Make a new object and return it
-			$className = $regDrivers[ strtolower( $driver ) ]['class'];
-			return new $className( $session_var, $options );
+			// Initializes YDBase
+			$this->YDBase();
 			
-				
-		}
-
-		/**
-		 *	The real constructor for the cart controller.
-		 *
-		 *	@param $session_var		(optional) Name to use in the session var - $_SESSION[ $session_var ]
-		 *	@param $options			(optional) Information used by the cart driver.
-		 *
-		 *	@internal		 
-		 */		
-		function _initCart( $session_var = 'YDCart', $options=array() ) {
-		
 			if ( ! isset( $_SESSION[$session_var] ) ) {
 				$_SESSION[$session_var] = & $this->item;	
 			} else {			
 				$this->item = & $_SESSION[$session_var];
 			}
-			
-			$this->_options = $options;
 		
 		}
 
@@ -106,6 +47,7 @@
 		function addItem( $id, $quantity=1 ) {
 		
 			return $this->setItem( $id, $quantity, true );
+		
 		}
 		
 		/**
@@ -123,8 +65,7 @@
 					$result = $this->item[$id] - (int) $quantity;
 					
 					if ( $result > 0 ) {
-						$this->item[$id] = $result;
-						return true;
+						return $this->item[$id] = $result;
 					}
 					
 				}
@@ -145,43 +86,23 @@
 		 *	@param $quantity	Number of items to set.
 		 *	@param $add			(optional) Boolean: true - $quantity will be added to the current quantity.
 		 *											false - $quantity will be the new quantity for that product.
+		 *
+		 *  @returns 			Current number of items or the result.
 		 */			
 		function setItem( $id, $quantity, $add=false ) {
 		
-			$quantity = $this->_getAvailable( $id, $quantity );
-			
 			if ( $add && $this->inCart( $id ) ) {
 				$quantity += $this->item[$id];
 			}
 			
 			if ( $quantity > 0 ) {
-				$this->item[$id] = $quantity;
-				return true;
+				return $this->item[$id] = $quantity;
 			} else {
 				return $this->remItem( $id );
 			}
 		
 		}
 
-		/**
-		 *	This function compares if the quantity needed is available.
-		 *
-		 *	@param $id			Product ID.
-		 *	@param $quantity	Quantity needed of the product.
-		 *
-		 *  @returns 			The available quantity.
-		 *
-		 *	@internal
-		 */			
-		function _getAvailable( $id, $quantity ) {
-
-			$available = $this->getQuantity( $id );
-			if ( $quantity >= $available ) {
-				$quantity = $available;
-			}			
-			return $quantity;
-		
-		}
 
 		/**
 		 *	This function will return the array of items of the basket.
@@ -239,144 +160,22 @@
 		}
 
 		/**
-		 *	This function will return the total cost of the cart.
-		 *
-		 *  @returns	The total cost of the cart.
-		 */			
-		function getTotal() {
-		
-			$total = 0;
-			
-			foreach ( $this->item as $id => $quantity ) {
-				$total += $this->getPrice( $id ) * $quantity;
-			}
-			
-			return $total;
-			
-		}
-
-		/**
-		 *	This function will return the total cost of an item in the cart.
+		 *	This function will return the item's quantity in the cart.
 		 *
 		 *  @param $id	Product ID.
 		 *
-		 *  @returns	The total cost of an item.
-		 */				
-		function getItemTotal( $id ) {
-		
-			$total = 0;
-			
-			if ( $this->inCart( $id ) ) {			
-				$total = $this->getPrice( $id ) * $this->item[$id];					
-			}
-			
-			return $total;
-				
-		}
-
-		/**
-		 *	This function will return the item's quantity in the cart.
-		 *
 		 *  @returns	The item's quantity in the cart.
 		 */				
-		function getItemQuantity( $id ) {
+		function getItemCount( $id ) {
 		
-			$quantity = 0;
-			
-			if ( $this->inCart( $id ) ) {			
-				$quantity = $this->item[$id];					
+			if ( $this->inCart( $id ) ) {
+				return $this->item[$id];					
 			}
 			
-			return $quantity;
+			return 0;
 				
 		}		
 	
-	}
-
-	/**
-	 *	This class defines a cart driver.
-	 */	
-	class YDCartDriver extends YDCart {
-
-		/**
-		 *	Class constructor.
-		 *
-		 *	@param $session_var		(optional) Name to use in the session var - $_SESSION[ $session_var ]
-		 *	@param $options			(optional) Information used by the cart driver.
-		 */			
-		function YDCartDriver( $session_var = 'YDCart', $options=array() ) {			
-			$this->_initCart( $session_var, $options );			
-		}
-
-		/**
-		 *	This function will return all the product's information.
-		 *
-		 *	@param $id		Product ID.
-		 *
-		 *  @returns		An array with the information or false if an error ocurred.
-		 */			
-		function getInfo( $id ) {
-		
-			return array();
-		
-		}
-
-		/**
-		 *	This function will return the product's name.
-		 *
-		 *	@param $id		Product ID.
-		 */			
-		function getName( $id ) {
-		}
-		
-		/**
-		 *	This function will return the product's price.
-		 *
-		 *	@param $id		Product ID.
-		 */			
-		function getPrice( $id ) {
-		}
-		
-		/**
-		 *	This function will return the product's description.
-		 *
-		 *	@param $id		Product ID.
-		 */			
-		function getDescription( $id ) {
-		}
-		
-		/**
-		 *	This function will return the product's quantity available.
-		 *
-		 *	@param $id		Product ID.
-		 */			
-		function getQuantity( $id ) {			
-		}
-
-		/**
-		 *	This function will return the product's thumbnail location.
-		 *
-		 *	@param $id		Product ID.
-		 */			
-		function getThumbnail( $id ) {
-		}
-
-		/**
-		 *	This function will return the product's picture location.
-		 *
-		 *	@param $id		Product ID.
-		 */					
-		function getPicture( $id ) {
-		}
-		
-		/**
-		 *	This function will return the product's shipping information.
-		 *
-		 *	@param $id		Product ID.
-		 */			
-		function getShipping( $id ) {
-		}
-		
 	}
 
 ?>
