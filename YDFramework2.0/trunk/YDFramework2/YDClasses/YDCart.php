b2fb7623803c2800abca00658d09aa8d387d0c10
david <david@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2004-10-05 18:41:06 +0000

    Fixed another bug in the documentation for the constructor of the YDCart class.

--- YDFramework2.0/trunk/YDFramework2/YDClasses/YDCart.php
+++ YDFramework2.0/trunk/YDFramework2/YDClasses/YDCart.php
@@ -21,8 +21,6 @@
 		 *	Class constructor for the YDCart class.
 		 *
 		 *	@param $session_var		(optional) Name to use in the session var - $_SESSION[ $session_var ]
-		 *
-		 *	@deprecated	 			Use the static method getInstance to get a new YDCart class instance.
 		 */			
 		function YDCart( $session_var='YDCart' ) {			
 		
