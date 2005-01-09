a4d1007cd6149e3750f67d20a134df93b490c46d
pieter <pieter@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2005-01-09 10:54:31 +0000

    Fixed some bugs with optional parameters in the validation rules that had no default in the function call.
    More info: http://www.yellowduck.be/ydf2/forum/viewtopic.php?t=205

--- YDFramework2.0/trunk/YDFramework2/YDClasses/YDValidateRules.php
+++ YDFramework2.0/trunk/YDFramework2/YDClasses/YDValidateRules.php
@@ -1,24 +1,24 @@
 <?php
 
 	/*
-	
+
 		Yellow Duck Framework version 2.0
 		Copyright (C) (c) copyright 2004 Pieter Claerhout
-	
+
 		This library is free software; you can redistribute it and/or
 		modify it under the terms of the GNU Lesser General Public
 		License as published by the Free Software Foundation; either
 		version 2.1 of the License, or (at your option) any later version.
-	
+
 		This library is distributed in the hope that it will be useful,
 		but WITHOUT ANY WARRANTY; without even the implied warranty of
 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 		Lesser General Public License for more details.
-	
+
 		You should have received a copy of the GNU Lesser General Public
 		License along with this library; if not, write to the Free Software
 		Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-	
+
 	*/
 
 	if ( ! defined( 'YD_FW_NAME' ) ) {
@@ -30,7 +30,7 @@
 	 */
 	class YDValidateRules extends YDBase {
 
-		/** 
+		/**
 		 *	This function returns false if the variable is empty, otherwise, it returns true.
 		 *
 		 *	@param $val		The value to test.
@@ -44,8 +44,8 @@
 			}
 		}
 
-		/** 
-		 *	This function returns true if the variable is smaller than the specified length, otherwise, it returns 
+		/**
+		 *	This function returns true if the variable is smaller than the specified length, otherwise, it returns
 		 *	false.
 		 *
 		 *	@param $val		The value to test.
@@ -59,8 +59,8 @@
 			}
 		}
 
-		/** 
-		 *	This function returns true if the variable is bigger than the specified length, otherwise, it returns 
+		/**
+		 *	This function returns true if the variable is bigger than the specified length, otherwise, it returns
 		 *	false.
 		 *
 		 *	@param $val		The value to test.
@@ -74,7 +74,7 @@
 			}
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the length of the variable is contained in the indicated range.
 		 *
 		 *	@param $val		The value to test.
@@ -88,8 +88,8 @@
 			}
 		}
 
-		/** 
-		 *	This function returns true if the variable matches the given regular expression (PCRE syntax), otherwise, it  
+		/**
+		 *	This function returns true if the variable matches the given regular expression (PCRE syntax), otherwise, it
 		 *	returns false.
 		 *
 		 *	@param $val		The value to test.
@@ -103,23 +103,23 @@
 			}
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable is a correctly formatted email address.
 		 *
 		 *	@param $val		The value to test.
 		 *	@param $opts	(not required)
 		 */
-		function email( $val, $opts ) {
+		function email( $val, $opts='' ) {
 			return YDValidateRules::regex( $val, '/^((\"[^\"\f\n\r\t\v\b]+\")|([\w\!\#\$\%\&\'\*\+\-\~\/\^\`\|\{\}]+(\.[\w\!\#\$\%\&\'\*\+\-\~\/\^\`\|\{\}]+)*))@((\[(((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9])))\])|(((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9]))\.((25[0-5])|(2[0-4][0-9])|([0-1]?[0-9]?[0-9])))|((([A-Za-z0-9\-])+\.)+[A-Za-z\-]+))$/' );
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable contains only letters.
 		 *
 		 *	@param $val		The value to test.
 		 *	@param $opts	(not required)
 		 */
-		function lettersonly( $val, $opts ) {
+		function lettersonly( $val, $opts='' ) {
 			$result = YDValidateRules::regex( $val, '/([\D^ ]+)$/' );
 			if ( $result === true ) {
 				$result = YDValidateRules::nopunctuation( $val, array() ) ? true : false;
@@ -127,23 +127,23 @@
 			return $result;
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable contains only single character.
 		 *
 		 *	@param $val		The value to test.
 		 *	@param $opts	(not required)
 		 */
-		function character( $val, $opts ) {
+		function character( $val, $opts='' ) {
 			return ( strlen( strval( $val ) ) == 1 ) && YDValidateRules::lettersonly( $val, array() );
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable contains only letters and numbers.
 		 *
 		 *	@param $val		The value to test.
 		 *	@param $opts	(not required)
 		 */
-		function alphanumeric( $val, $opts ) {
+		function alphanumeric( $val, $opts='' ) {
 			$result = YDValidateRules::regex( $val, '/([\w^ ]+)$/' );
 			if ( $result === true ) {
 				$result = YDValidateRules::nopunctuation( $val, array() ) ? true : false;
@@ -151,37 +151,37 @@
 			return $result;
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable contains only numbers.
 		 *
 		 *	@param $val		The value to test.
 		 *	@param $opts	(not required)
 		 */
-		function numeric( $val, $opts ) {
+		function numeric( $val, $opts='' ) {
 			return YDValidateRules::regex( $val, '/(^-?\d\d*\.\d*$)|(^-?\d\d*$)|(^-?\.\d\d*$)/' );
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable contains only contains one digit (0-9).
 		 *
 		 *	@param $val		The value to test.
 		 *	@param $opts	(not required)
 		 */
-		function digit( $val, $opts ) {
+		function digit( $val, $opts='' ) {
 			return ( strlen( strval( $val ) ) == 1 ) && YDValidateRules::numeric( $val, array() );
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable contains no punctuation characters.
 		 *
 		 *	@param $val		The value to test.
 		 *	@param $opts	(not required)
 		 */
-		function nopunctuation( $val, $opts ) {
+		function nopunctuation( $val, $opts='' ) {
 			return YDValidateRules::regex( $val, '/^[^().\/\*\^\?#!@$%+=,\"\'><~\[\]{}]+$/' );
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable is a number not starting with 0.
 		 *
 		 *	@param $val		The value to test.
@@ -190,7 +190,7 @@
 		 *	@todo
 		 *		Allows things other than digits
 		 */
-		function nonzero( $val, $opts ) {
+		function nonzero( $val, $opts='' ) {
 			$result = YDValidateRules::regex( $val, '/^-?[1-9][0-9]*/' );
 			if ( $result === true ) {
 				$result = YDValidateRules::numeric( $val, array() ) ? true : false;
@@ -198,37 +198,37 @@
 			return $result;
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable is exactly as specified in the options.
 		 *
 		 *	@param $val		The value to test.
-		 *	@param $opts	(not required)
+		 *	@param $opts	The value to compare with.
 		 */
 		function exact( $val, $opts ) {
-			return $val === $opts;;
+			return $val === $opts;
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable is in the array specified in the options.
 		 *
 		 *	@param $val		The value to test.
-		 *	@param $opts	(not required)
+		 *	@param $opts	The array in which the value should be.
 		 */
 		function in_array( $val, $opts ) {
 			return in_array( $val, $opts, true );
 		}
 
-		/** 
+		/**
 		 *	This function returns true if the variable is not in the array specified in the options.
 		 *
 		 *	@param $val		The value to test.
-		 *	@param $opts	(not required)
+		 *	@param $opts	The array in which the value should not be.
 		 */
 		function not_in_array( $val, $opts ) {
 			return ! in_array( $val, $opts, true );
 		}
 
-		/** 
+		/**
 		 *	This rule allows to use an external function/method for validation, either by registering it or by passing a
 		 *	callback as a format parameter.
 		 *
@@ -239,17 +239,17 @@
 			return call_user_func( $opts, $val );
 		}
 
-		/** 
+		/**
 		 *	This rule checks if a file was uploaded.
 		 *
 		 *	@param $val		The value to test.
 		 *	@param $opts	(not required)
 		 */
-		function uploadedfile( $val, $opts ) {
+		function uploadedfile( $val, $opts='' ) {
 			return is_uploaded_file( $val['tmp_name'] ) && ( filesize( $val['tmp_name'] ) > 0 );
 		}
 
-		/** 
+		/**
 		 *	This rule checks if a file upload exceeded the file size or not.
 		 *
 		 *	@param $val		The value to test.
@@ -259,7 +259,7 @@
 			return ( filesize( $val['tmp_name'] ) <= intval( $opts ) );
 		}
 
-		/** 
+		/**
 		 *	This rule checks if a file upload had the right mime type.
 		 *
 		 *	@param $val		The value to test.
@@ -272,7 +272,7 @@
 			return in_array( $val['type'], $opts );
 		}
 
-		/** 
+		/**
 		 *	This rule checks if a file upload had the filename based on a regular expression.
 		 *
 		 *	@param $val		The value to test.
@@ -282,7 +282,7 @@
 			return YDValidateRules::regex( $val['name'], $opts );
 		}
 
-		/** 
+		/**
 		 *	This rule checks if a file upload has the right file extension.
 		 *
 		 *	@param $val		The value to test.
