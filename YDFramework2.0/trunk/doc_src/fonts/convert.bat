fd3ccc6020a3bef6ff09defce3230ec97d6abfd5
pieter <pieter@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2005-01-14 21:29:14 +0000

    Updated the installation chapter.

--- YDFramework2.0/trunk/doc_src/fonts/convert.bat
+++ YDFramework2.0/trunk/doc_src/fonts/convert.bat
@@ -5,3 +5,4 @@ set CLASSPATH=%CLASSPATH%;C:\bin\fop\build\fop.jar;C:\bin\fop\lib\xercesImpl.jar
 java org.apache.fop.fonts.apps.TTFReader -enc ansi LSANS.TTF lucida_sans.xml
 java org.apache.fop.fonts.apps.TTFReader -enc ansi LSANSD.TTF lucida_sans-bold.xml
 java org.apache.fop.fonts.apps.TTFReader -enc ansi LTYPE.TTF lucida_typewriter.xml
+java org.apache.fop.fonts.apps.TTFReader -enc ansi LTYPEB.TTF lucida_typewriter-bold.xml
