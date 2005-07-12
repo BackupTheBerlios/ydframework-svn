d2946fa52b77440d7f89474fa5d4db2578e42d56
pclaerhout <pclaerhout@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2005-07-12 18:55:07 +0000

    Made the Yellow Duck Framework work properly under a CGI environment.
    More info: http://www.yellowduck.be/ydf/forum/viewtopic.php?t=373

--- YDFramework2.0/trunk/YDFramework2/YDF2_init.php
+++ YDFramework2.0/trunk/YDFramework2/YDF2_init.php
@@ -84,7 +84,8 @@
     @define( 'YD_ACTION_DEFAULT', 'actionDefault' );
 
     // File and URL constants
-    @define( 'YD_SELF_SCRIPT', $_SERVER['SCRIPT_NAME'] );
+    //@define( 'YD_SELF_SCRIPT', $_SERVER['SCRIPT_NAME'] );
+    @define( 'YD_SELF_SCRIPT', $PHP_SELF );
     if ( ! isset( $_SERVER['SCRIPT_FILENAME'] ) ) {
         $_SERVER['SCRIPT_FILENAME'] = $_SERVER['PATH_TRANSLATED'];
     }
