d96a97a8f487694f0106fa5ed987191f4e3d9c4b
pclaerhout <pclaerhout@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2005-07-13 19:36:22 +0000

    Added the weblog_dir template variable.

--- YDFramework2.0/trunk/examples/weblog/include/YDWeblogRequest.php
+++ YDFramework2.0/trunk/examples/weblog/include/YDWeblogRequest.php
@@ -104,6 +104,7 @@
             // Assign the weblog details
             $this->tpl->assign( 'weblog_title',                YDConfig::get( 'weblog_title', 'Untitled Weblog' ) );
             $this->tpl->assign( 'weblog_description',          YDConfig::get( 'weblog_description', 'Untitled Weblog Description' ) );
+            $this->tpl->assign( 'weblog_dir',                  YDUrl::makeLinkAbsolute( '.' ) );
             $this->tpl->assign( 'weblog_link',                 YDUrl::makeLinkAbsolute( 'index.php' ) );
             $this->tpl->assign( 'weblog_link_rss',             YDUrl::makeLinkAbsolute( 'xml.php?do=rss' ) );
             $this->tpl->assign( 'weblog_link_atom',            YDUrl::makeLinkAbsolute( 'xml.php?do=atom' ) );
