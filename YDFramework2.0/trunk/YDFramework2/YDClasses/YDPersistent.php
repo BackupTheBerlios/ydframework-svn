7153572b3f877e24ad179c0dae119de33af9d9d0
pclaerhout <pclaerhout@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2005-07-16 09:39:39 +0000

    Added an initial version of the YDPersistent class.
    More info:
    http://www.yellowduck.be/ydf/forum/viewtopic.php?t=397

---
+++ YDFramework2.0/trunk/YDFramework2/YDClasses/YDPersistent.php
@@ -0,0 +1,180 @@
+<?php
+
+    /*
+
+        Yellow Duck Framework version 2.0
+        (c) Copyright 2002-2005 Pieter Claerhout
+
+        This library is free software; you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public
+        License as published by the Free Software Foundation; either
+        version 2.1 of the License, or (at your option) any later version.
+
+        This library is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+        Lesser General Public License for more details.
+
+        You should have received a copy of the GNU Lesser General Public
+        License along with this library; if not, write to the Free Software
+        Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+    */
+
+    // Check if the framework is loaded
+    if ( ! defined( 'YD_FW_NAME' ) ) {
+        die( 'Yellow Duck Framework is not loaded.' );
+    }
+
+    // Includes
+    YDInclude( 'YDEncryption.php' );
+
+    // Constants
+    YDConfig::set( 'YD_PERSISTENT_STORE_NAME', strtoupper( str_replace( ' ', '_', YD_FW_NAME ) ) . '_PERSISTENT_STORE' );
+
+    /**
+     *  This class is able to save and load persistent data. This data stay active between different requests and allows
+     *  you to share data between different requests and different sessions.
+     */
+    class YDPersistent extends YDBase {
+
+        /**
+         *	This function initializes the persistent object store.
+         *
+         *	@internal
+         */
+        function _init() {
+            if ( ! isset( $_COOKIE[ YDConfig::get( 'YD_PERSISTENT_STORE_NAME' ) ] ) ) {
+                $_COOKIE[ YDConfig::get( 'YD_PERSISTENT_STORE_NAME' ) ] = array();
+            }
+        }
+
+        /**
+         *	This funtion add a new object to the persistent object store.
+         *
+         *	@param	$name		The name of the object.
+         *	@param	$object		The object to store.
+         *  @param  $passwd     (optional) If not null, the data will be encrypted with this password.
+         *  @param  $expire     (optional) The lifetime of the object in seconds. Defaults to 1 year.
+         *  @param  $scope      (optional) The scope of the object in seconds. Defaults to "/".
+         */
+        function set( $name, $object, $passwd=null, $expire=null, $scope='/' ) {
+
+            // Initialize the store
+            YDPersistent::_init();
+
+            // Set the expire time
+            if ( is_null( $expire ) ) {
+                $expire = time() + 31536000;
+            }
+
+            // Convert the object to a base64 encoded serialized version
+            $object_data = base64_encode( serialize( $object ) );
+
+            // Encrypt the data if needed
+            if ( ! is_null( $passwd ) ) {
+                $object_data = YDEncryption::encrypt( $passwd, $object_data );
+            }
+
+            // Save the object
+            $_COOKIE[ YDConfig::get( 'YD_PERSISTENT_STORE_NAME' ) ][$name] = $object_data;
+            setcookie( YDConfig::get( 'YD_PERSISTENT_STORE_NAME' ) . '[' . $name . ']', $object_data, $expire, $scope );
+
+        }
+
+        /**
+         *	This function returns a variable from the configuration. If the configuration variable doesn't exist, it
+         *	returns a fatal error.
+         *
+         *	@param	$name       The name of the configuration variable to retrieve.
+         *  @param  $default    (optional) If not null, this value will be returned if the configuration setting doesn't
+         *                      exist in the configuration.
+         *  @param  $passwd     (optional) If not null, the data will be decrypted with this password.
+         *
+         *	@returns	The value of the configuration variable.
+         */
+        function get( $name, $default=null, $passwd=null ) {
+
+            // Initialize the store
+            YDPersistent::_init();
+
+            // If the $_GET variable exists, return that one
+            if ( isset( $_GET[$name] ) === true ) {
+                YDPersistent::set( $name, $_GET[$name], $passwd );
+            }
+
+            // Check if the key exists
+            if ( ! YDPersistent::exists( $name ) ) {
+
+                // Check if we have a default, if not, raise an error
+                if ( ! is_null( $default ) ) {
+                    return $default;
+                } else {
+                    trigger_error( 'Persistent variable "' . $name . '" is not defined.', YD_ERROR );
+                }
+
+            }
+
+            // Get the value
+            $obj = $_COOKIE[ YDConfig::get( 'YD_PERSISTENT_STORE_NAME' ) ][ $name ];
+
+            // Decrypt the data if needed
+            if ( ! is_null( $passwd ) ) {
+                $obj = YDEncryption::decrypt( $passwd, $obj );
+            }
+
+            // Now, we need to base64 decode and unserialize
+            $obj = @ unserialize( base64_decode( $obj ) );
+
+            // Return the object
+            return $obj;
+
+        }
+
+        /**
+         *  This function removes a persistent variable.
+         *
+         *	@param	$name		The name of the object.
+         */
+        function delete( $name ) {
+
+            // Initialize the store
+            YDPersistent::_init();
+
+            // Remove it from the cookie variable
+            unset( $_COOKIE[ $name ] );
+            setcookie( YDConfig::get( 'YD_PERSISTENT_STORE_NAME' ) . '[' . $name . ']', " ", time()-3600 );
+
+        }
+
+        /**
+         *	This function checks if the configuration value is set or not.
+         *
+         *	@returns	Boolean indicating if the configuration value is set or not.
+         */
+        function exists( $name ) {
+
+            // Initialize the store
+            YDPersistent::_init();
+
+            // Return true or false
+            return isset( $_COOKIE[ YDConfig::get( 'YD_PERSISTENT_STORE_NAME' ) ][ $name ] );
+
+        }
+
+        /**
+         *	This function dumps the contents of the configuration.
+         */
+        function dump() {
+
+            // Initialize the store
+            YDPersistent::_init();
+
+            // Dump the configuration
+            YDDebugUtil::dump( $_COOKIE[ YDConfig::get( 'YD_PERSISTENT_STORE_NAME' ) ] );
+
+        }
+
+    }
+
+?>
