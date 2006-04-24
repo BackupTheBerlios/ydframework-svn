daf664bc8928c36cadaee0075f3426c342d8e710
ximian <ximian@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2006-04-24 10:16:25 +0000

    added new generic languages to the master languages component

--- YDFramework2.0/trunk/YDFramework2/addons/YDCMLanguages/languages/en.php
+++ YDFramework2.0/trunk/YDFramework2/addons/YDCMLanguages/languages/en.php
@@ -6,5 +6,8 @@ $GLOBALS['t']['open'] = 'Open';
 $GLOBALS['t']['help'] = 'Help';
 $GLOBALS['t']['back'] = 'Back';
 $GLOBALS['t']['not applicable'] = 'Not applicable';
+$GLOBALS['t']['yes'] = 'Yes';
+$GLOBALS['t']['no'] = 'No';
+$GLOBALS['t']['schedule'] = 'Schedule';
 
 ?>
--- YDFramework2.0/trunk/YDFramework2/addons/YDCMLanguages/languages/pt.php
+++ YDFramework2.0/trunk/YDFramework2/addons/YDCMLanguages/languages/pt.php
@@ -6,5 +6,8 @@ $GLOBALS['t']['open'] = 'Abrir';
 $GLOBALS['t']['help'] = 'Ajuda';
 $GLOBALS['t']['back'] = 'Voltar';
 $GLOBALS['t']['not applicable'] = 'N&atilde;o aplic&aacute;vel';
+$GLOBALS['t']['yes'] = 'Sim';
+$GLOBALS['t']['no'] = 'N&atilde;o';
+$GLOBALS['t']['schedule'] = 'Calend&aacute;rio';
 
 ?>
