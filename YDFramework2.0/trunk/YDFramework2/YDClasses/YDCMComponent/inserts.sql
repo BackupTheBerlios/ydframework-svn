85c294c43f3c8f1a7b0a158221fcde3ebba5ac2f
ximian <ximian@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2006-04-24 13:49:30 +0000

    - added rows about YDCMPage components and a new language type 'all' that identifies all language types

--- YDFramework2.0/trunk/YDFramework2/YDClasses/YDCMComponent/inserts.sql
+++ YDFramework2.0/trunk/YDFramework2/YDClasses/YDCMComponent/inserts.sql
@@ -12,6 +12,7 @@ INSERT INTO `ydcmconfiguration_files` VALUES ('video','mpg, mpeg, avi, wmv');
 
 
 
+INSERT INTO `ydcmlanguages` VALUES ('all','All',1,0);
 INSERT INTO `ydcmlanguages` VALUES ('en','English',1,1);
 INSERT INTO `ydcmlanguages` VALUES ('pt','Português',1,0);
 
@@ -30,4 +31,16 @@ INSERT INTO `ydcmtree` VALUES (9,8,15,16,3,1,'YDCMPage','databases',1,1,1,0,0);
 INSERT INTO `ydcmtree` VALUES (10,8,17,18,3,1,'YDCMPage','generators',1,1,1,0,0);
 INSERT INTO `ydcmtree` VALUES (11,8,19,20,3,1,'YDCMPage','portals',1,1,1,0,0);
 
+
+INSERT INTO `ydcmpage` VALUES (1,  5, 'en', 1, 'Apache in english', '', '', 0, '', 0, '', '');
+INSERT INTO `ydcmpage` VALUES (2,  6, 'all', 1, 'PostgreSQL in all lang', '', '', 0, '', 0, '', '');
+INSERT INTO `ydcmpage` VALUES (3,  7, 'pt', 1, 'mySQL in PT', '', '', 0, '', 0, '', '');
+INSERT INTO `ydcmpage` VALUES (4,  9, 'en', 1, 'Our databases', '', '', 0, '', 0, '', '');
+INSERT INTO `ydcmpage` VALUES (5, 10, 'en', 1, 'Generators', '', '', 0, '', 0, '', '');
+INSERT INTO `ydcmpage` VALUES (6, 11, 'all', 1, 'Portals for all', '', '', 0, '', 0, '', '');
+
+
 INSERT INTO `ydcmusers` VALUES (2,0,0,0,0,'admin','21232f297a57a5a743894a0e4a801fc3','name','email@test.com','a','en',1,NULL,NULL,7,'2006-04-23 17:02:27','2006-04-23 17:02:34',0,'0000-00-00 00:00:00');
+
+
+
