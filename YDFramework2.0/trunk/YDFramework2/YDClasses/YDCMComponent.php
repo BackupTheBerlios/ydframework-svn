25623679de9f08958c8b602daeb47193f2847639
ximian <ximian@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2006-05-08 14:25:01 +0000

    updated YDCMComponent for new structure and db scheme

--- YDFramework2.0/trunk/YDFramework2/YDClasses/YDCMComponent.php
+++ YDFramework2.0/trunk/YDFramework2/YDClasses/YDCMComponent.php
@@ -26,7 +26,15 @@
         die( 'Yellow Duck Framework is not loaded.' );
     }
 
+	// include YDF libs
     YDInclude( 'YDDatabaseObject.php' );
+	YDInclude( 'YDDatabaseTree.php' );
+
+	// include core YDCM libs
+	require_once( dirname( __FILE__ ) . '/YDCMComponent/YDCMUsers/YDCMUsers.php' );
+	require_once( dirname( __FILE__ ) . '/YDCMComponent/YDCMTree/YDCMTree.php' );
+	require_once( dirname( __FILE__ ) . '/YDCMComponent/YDCMTemplates/YDCMTemplates.php' );
+	require_once( dirname( __FILE__ ) . '/YDCMComponent/YDCMLanguages/YDCMLanguages.php' );
 
 
     class YDCMComponent extends YDDatabaseObject {
@@ -237,68 +245,5 @@
 
 
     }
-
-
-
-
-    class YDCMTree extends YDDatabaseObject {
-    
-        function YDCMTree() {
-        
-			// init component as non default
-            $this->YDDatabaseObject();
-			
-			// register database as default
-            $this->registerDatabase();
-
-			// register table for this component
-            $this->registerTable( 'YDCMTree' );
-
-            // register custom key
-            $this->registerKey( 'content_id', true );
-
-			// register fields	
-			$this->registerField( 'parent_id' );
-			$this->registerField( 'type' );
-			$this->registerField( 'state' );
-			$this->registerField( 'reference' );
-			$this->registerField( 'access' );			
-			$this->registerField( 'searcheable' );			
-			$this->registerField( 'published_date_start' );
-			$this->registerField( 'published_date_end' );			
-			$this->registerField( 'candrag' );
-			$this->registerField( 'candrop' );
-		}
-
-
-        /**
-         *  This function checks if elements are valid drag&dropable
-         *
-         *  @param $x  Id of dragable node
-         *
-         *  @param $y  Id of dropable node
-
-         *  @returns    false if elements are invalid, an associative array with node information
-         */
-		function getDragDropElements( $x, $y ){
-		
-			// check if elements are numeric
-			if ( !is_numeric( $x ) || !is_numeric( $y ) ) return false;
-		
-			// reset old values and get current table	
-			$this->resetValues();
-
-			// set custom where to get those 2 elements
-			$this->where( '((content_id = ' . intval( $x ) . ' AND candrag = 1) OR (content_id = ' . intval( $y ) . ' AND candrop = 1))' );
-
-			// we must get always 2 elements otherwise they were not valid
-			if ( $this->find() != 2 ) return false;
-
-			// return associative array
-			return $this->getResultsAsAssocArray( 'content_id' );
-		}
-
-
-	}
 
 ?>
