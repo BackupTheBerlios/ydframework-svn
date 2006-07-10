ac8fd3e96b441a042a4f26da67be57211abbcc25
pclaerhout <pclaerhout@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2006-07-10 13:44:30 +0000

    The number of items and cache size are now listed in the manage cache section.

--- YDFramework2.0/trunk/examples/weblog/manage/cache.php
+++ YDFramework2.0/trunk/examples/weblog/manage/cache.php
@@ -17,14 +17,21 @@
         // Default action
         function actionDefault() {
 
+            // Get the list of files and file sizes
+            $cache_pub_lbl = 'Public cache ' . $this->_getTotalSizeAndCountAsText( YD_WEBLOG_CACHE_PREFIX . '*.' . YD_WEBLOG_CACHE_SUFFIX );
+            $cache_tmb_lbl = 'Thumbnail cache ' . $this->_getTotalSizeAndCountAsText( YD_TMP_PRE . 'N_*.*' );
+            $cache_web_lbl = 'Web download cache ' . $this->_getTotalSizeAndCountAsText( '*.wch' );
+            $cache_tpl_lbl = 'Template cache ' . $this->_getTotalSizeAndCountAsText( '*.tpl.php' );
+
             // Create the delete form
             $form = new YDForm( 'clearCacheForm' );
-            $form->addElement( 'checkbox', 'cache_pub', 'Public cache', array( 'style' => 'border: none;' ) );
-            $form->addElement( 'checkbox', 'cache_tmb', 'Thumbnail cache', array( 'style' => 'border: none;' ) );
-            $form->addElement( 'checkbox', 'cache_web', 'Web download cache', array( 'style' => 'border: none;' ) );
-            $form->addElement( 'checkbox', 'cache_tpl', 'Template cache', array( 'style' => 'border: none;' ) );
+            $form->addElement( 'checkbox', 'cache_pub', $cache_pub_lbl, array( 'style' => 'border: none;' ) );
+            $form->addElement( 'checkbox', 'cache_tmb', $cache_tmb_lbl, array( 'style' => 'border: none;' ) );
+            $form->addElement( 'checkbox', 'cache_web', $cache_web_lbl, array( 'style' => 'border: none;' ) );
+            $form->addElement( 'checkbox', 'cache_tpl', $cache_tpl_lbl, array( 'style' => 'border: none;' ) );
             $form->addElement( 'submit', '_cmdSubmit', t( 'cleanup' ), array( 'class' => 'button' ) );
             $form->setDefaults( array( 'cache_pub' => 1, 'cache_web' => 1, 'cache_tmb' => 1, 'cache_tpl' => 1 ) );
+
 
             // Validate the form
             if ( $form->validate() == true ) {
@@ -64,11 +71,34 @@
 
         // Helper function to delete files
         function _deleteCacheFiles( $pattern ) {
-            $dir = new YDFSDirectory( YD_DIR_TEMP );
-            $filesToDelete = $dir->getContents( $pattern, null );
-            foreach ( $filesToDelete as $file ) {
+            $files = $this->_getFileList( $pattern );
+            foreach ( $files as $file ) {
                 @unlink( $file->getAbsolutePath() );
             }
+        }
+
+        // Helper function to get the total file size and number of files
+        function _getTotalSizeAndCount( $pattern ) {
+            $count = 0;
+            $size  = 0;
+            $files = $this->_getFileList( $pattern );
+            foreach ( $files as $file ) {
+                $count++;
+                $size += $file->getSize();
+            }
+            return array( $count, $size );
+        }
+
+        // Get the total size and count as a piece of text
+        function _getTotalSizeAndCountAsText( $pattern ) {
+            $res = $this->_getTotalSizeAndCount( $pattern );
+            return sprintf( '(%s %s, %s)', $res[0], t('items'), YDStringUtil::formatFileSize( $res[0] ) );
+        }
+
+        // Get a filelist
+        function _getFileList( $pattern ) {
+            $dir = new YDFSDirectory( YD_DIR_TEMP );
+            return $dir->getContents( $pattern, null );
         }
 
     }
