97cebf0217d7ed69aa6ed312483a18e7810cc3c8
ximian <ximian@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2006-08-15 17:04:22 +0000

    added first YDCM example about user details changing

---
+++ YDFramework2.0/trunk/examples/cm/cm.php
@@ -0,0 +1,32 @@
+<?php
+
+    // Initialize the Yellow Duck Framework
+    include_once( dirname( __FILE__ ) . '/../../YDFramework2/YDF2_init.php' );
+
+    // Includes
+    YDInclude( 'YDRequest.php' );
+    YDInclude( 'YDTemplate.php' );
+    YDInclude( 'YDDatabase.php' );
+    YDInclude( 'YDCMComponent.php' );
+
+    // set YDDatabase instance connection
+    YDDatabase::registerInstance( 'default', 'mysql', 'tppp', 'root', '', 'localhost' );
+
+	// set portal language. Currently you can user 'en' and 'pt'.
+	YDLocale::set( 'en' );
+
+	// set admin template path
+	YDConfig::set( 'YDCMTEMPLATES_ADMIN_PATH', dirname( __FILE__ ) . '/backend/templates' );
+
+    // Class definition for the index request
+    class cm extends YDRequest {
+
+        // Class constructor
+        function cm() {
+
+            $this->YDRequest();
+            $this->template = new YDTemplate();
+        }
+
+    }
+?>
