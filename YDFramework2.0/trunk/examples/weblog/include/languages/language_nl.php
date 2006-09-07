a3aef0d2d5557daae1cafdea5da42959b016d6f2
pclaerhout <pclaerhout@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2006-09-07 18:33:00 +0000

    Updated the language files.

--- YDFramework2.0/trunk/examples/weblog/include/languages/language_nl.php
+++ YDFramework2.0/trunk/examples/weblog/include/languages/language_nl.php
@@ -327,7 +327,7 @@ $GLOBALS['t']['no_images_yet'] = 'Nog geen foto\'s';
 $GLOBALS['t']['image'] = 'foto';
 $GLOBALS['t']['images'] = 'foto\'s';
 $GLOBALS['t']['item'] = 'Item';
-$GLOBALS['t']['edit_meta'] = 'metadata';
+$GLOBALS['t']['edit_meta'] = 'informatie';
 
 // manage/index.tpl
 $GLOBALS['t']['latest_items'] = 'Laatste items';
