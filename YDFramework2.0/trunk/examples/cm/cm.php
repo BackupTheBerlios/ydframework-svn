9426d4037fa35ac78e56da27f53083ec3f5daecf
ximian <ximian@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2006-09-14 09:14:27 +0000

    - added YDConfig parameter to cm.php that defines the url path to template screenshots.

--- YDFramework2.0/trunk/examples/cm/cm.php
+++ YDFramework2.0/trunk/examples/cm/cm.php
@@ -4,14 +4,17 @@
     include_once( dirname( __FILE__ ) . '/../../YDFramework2/YDF2_init.php' );
 
 	YDInclude( 'YDDatabase.php' );
+	YDInclude( 'YDUrl.php' );
 
-    // set YDDatabase instance connection
+    // BASIC CONFIGURATION: set YDDatabase instance connection
     YDDatabase::registerInstance( 'default', 'mysql', 'xpto', 'root', '', 'localhost' );
 
-	// set portal language. Currently you can user 'en' and 'pt'.
+    // BASIC CONFIGURATION: set portal language. Currently you can use 'en' and 'pt'.
 	YDLocale::set( 'en' );
+
 
 	// set admin template path
 	YDConfig::set( 'YDCMTEMPLATES_ADMIN_PATH', dirname( __FILE__ ) . '/backend/templates' );
 
+	YDConfig::set( 'YDCMTEMPLATES_ADMIN_URL', YDUrl::makeLinkAbsolute( './templates' ) );
 ?>
