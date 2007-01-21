7a338d454b7b68fce17fe04ed839720daf276b77
pclaerhout <pclaerhout@b3c4cd22-0df1-b34c-98c4-973d2c6617a1>
2007-01-21 21:06:01 +0000

    Added the YDSimpleCMS database schema.

---
+++ YDFramework2.0/trunk/examples/simplecms/includes/database/ydsimplecms.sql
@@ -0,0 +1,21 @@
+# HeidiSQL Dump 
+#
+# --------------------------------------------------------
+# Host:                 localhost
+# Database:             ydsimplecms
+# Server version:       4.1.22-community-nt
+# Server OS:            Win32
+# max_allowed_packet:   1048576
+# HeidiSQL version:     3.0 RC4 Revision: 334
+# --------------------------------------------------------
+
+/*!40100 SET CHARACTER SET latin1;*/
+
+
+#
+# Database structure for database 'ydsimplecms'
+#
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ ydsimplecms;
+
+USE ydsimplecms;
